75901a4b082d8ac2c7a3fa3de150e7c04dea4dba
ErrorErrorError <16653389+ErrorErrorError@users.noreply.github.com>
2023-12-07 13:57:43 -0800

    wip: fix cards in homescreen

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,44 @@
-import Image from 'next/image';
+import Image from "next/image"
+import Link from "next/link"
+import { FiExternalLink, FiChevronRight } from "react-icons/fi"
 
 type CardInfo = {
-  title: string;
-  description?: string;
-};
+  title: string
+  description?: string
+  link?: {
+    external: boolean
+    href: string
+    title: string
+  }
+}
 
 const cards: CardInfo[] = [
   {
-    title: 'Open Source',
-    description: 'Available on GitHub',
+    title: "Open Source",
+    description: "Fully open-source and ad-free, forever.",
+    link: {
+      external: true,
+      href: "https://github.com/Mochi-Team/mochi",
+      title: "Available on GitHub",
+    },
   },
-];
+  {
+    title: "External Modules",
+    description:
+      "Take control of your media by building and importing modules.",
+    link: {
+      external: false,
+      href: "/docs",
+      title: "Learn more",
+    },
+  },
+]
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between gap-12">
       {/* Hero */}
-      <section className="flex w-full flex-col gap-6">
+      <section className="flex w-full flex-col gap-8">
         {/* Hero Title */}
         <div className="flex flex-1 flex-col gap-1 text-center">
           <p className="text-2xl font-bold">
@@ -26,24 +48,39 @@ export default function Home() {
           {/* Hero Title Buttons */}
         </div>
 
-        {/* <ul className='grid grid-cols-2 w-full'>
-          { cards.map(c => <InfoCard info={c} />) }
-        </ul> */}
+        {/* Display app screenshots */}
+
+        <ul className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
+          {cards.map((c) => (
+            <InfoCard info={c} />
+          ))}
+        </ul>
       </section>
     </main>
-  );
+  )
 }
 
 const InfoCard = ({ info }: { info: CardInfo }) => {
   return (
-    <div
+    <li
       key={info.title}
-      className="flex aspect-[8/5] w-full flex-col rounded-3xl border border-white/10 bg-neutral-500/40 p-6 transition duration-300 ease-in-out"
+      className="flex flex-col w-full rounded-2xl border border-neutral-500/30 bg-neutral-500/10 transition duration-300 ease-in-out p-6"
     >
-      <div className="text-left text-white">
-        <p className="text-lg font-semibold">{info.title}</p>
-        <p className="text-sm font-normal">{info.description}</p>
-      </div>
-    </div>
-  );
-};
+      <p className="text-md font-bold">{info.title}</p>
+      <p className="text-sm font-normal">{info.description}</p>
+      {info.link ? (
+        <Link
+          className="mt-4 text-xs flex self-start items-center gap-2 flex-row rounded-full border border-neutral-500/30 bg-neutral-500/30 py-2 px-4"
+          target={info.link.external ? "_blank" : ""}
+          rel={info.link.external ? "noopener noreferrer" : ""}
+          href={info.link.href}
+        >
+          {info.link.title}
+          {info.link.external ? <FiExternalLink /> : <FiChevronRight /> }
+        </Link>
+      ) : (
+        <></>
+      )}
+    </li>
+  )
+}
